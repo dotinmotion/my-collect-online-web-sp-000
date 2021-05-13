40528f9787820240d501e0a044f732c18aff0feb
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-13 16:01:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(array)
+  i=0
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+  array
+end
+
+my_collect(array) {|x| x.split(" ").first}
 
